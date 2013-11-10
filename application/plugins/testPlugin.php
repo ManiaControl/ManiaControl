a34f1c6fa7e62b08250442ff9cf3cad0b4a50f3a
kremsy <lukas@esc-clan.net>
2013-11-10 10:05:02 +0100

    improved testplugin

--- application/plugins/testPlugin.php
+++ application/plugins/testPlugin.php
@@ -3,25 +3,33 @@
 namespace ManiaControl;
 
 class TestPlugin extends Plugin {
+    const AUTHOR = 'steeffeen';
+    const NAME = 'Test Plugin';
+    const VERSION = '1.0';
+    const DESCRIPTION = 'Test Plugin';
 
 	public function __construct(ManiaControl $maniaControl) {
 		$this->maniaControl = $maniaControl;
 	}
 
+
+
+
+    //abstract Methods from ParentClass (do not change them!)
 	public function getAuthor() {
-		return 'steeffeen';
+		return SELF::AUTHOR;
 	}
 
 	public function getName() {
-		return 'Test Plugin';
+		return SELF::NAME;
 	}
 
 	public function getVersion() {
-		return '1.0';
+		return SELF::VERSION;
 	}
 
 	public function getDescription() {
-		return 'Dummy plugin for testing plugin handling';
+		return SELF::DESCRIPTION;
 	}
 }
 
