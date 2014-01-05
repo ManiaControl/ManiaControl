0459a945cee8644e03f4a4eaa49b4aef80dfb561
kremsy <lukas@esc-clan.net>
2014-01-05 19:10:11 +0100

    mc settings tab

--- application/core/Configurators/ManiaControlSettings.php
+++ application/core/Configurators/ManiaControlSettings.php
@@ -7,6 +7,7 @@
 namespace ManiaControl\Configurators;
 
 
+use FML\Controls\Frame;
 use FML\Script\Script;
 use ManiaControl\ManiaControl;
 use ManiaControl\Players\Player;
@@ -50,8 +51,19 @@ class ManiaControlSettings implements ConfiguratorMenu{
 	 * @return \FML\Controls\Frame
 	 */
 	public function getMenu($width, $height, Script $script) {
-		var_dump($this->maniaControl->settingManager->getSettings());
-		// TODO: Implement getMenu() method.
+		$pagesId = 'ScriptSettingsPages';
+		$frame = new Frame();
+
+		/** @var  ManiaControl/SettingManager $this->maniaControl->settingManager */
+		$settings = $this->maniaControl->settingManager->getSettings();
+
+		foreach($settings as $setting){
+			var_dump($setting);
+
+
+		}
+
+		return $frame;
 	}
 
 	/**
