6717039e918668525279d20007fd657f24785d1b
kremsy <lukas@esc-clan.net>
2014-02-14 19:22:11 +0100

    dedi begin

---
+++ application/plugins/Dedimania/DedimaniaData.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lukas
+ * Date: 14.02.14
+ * Time: 17:16
+ */
+
+namespace Dedimania;
+
+/**
+ * Dedimania Structure
+ *
+ * @author kremsy & steeffeen
+ */
+class DedimaniaData {
+	/*public $streamBuffer;
+	public $socket;
+	public $function;
+	public $url;
+	public $creationTime;*/
+
+	public function __construct($url, $socket, $function) {
+		/*$this->url          = $url;
+		$this->socket       = $socket;
+		$this->function     = $function;
+		$this->creationTime = time();
+		$this->streamBuffer = '';*/
+	}
+} 
