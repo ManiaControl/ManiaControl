f399f4ee6f5197709683a94bbc71396f4339b166
Steffen Schröder <schteffens@gmail.com>
2014-04-27 21:31:55 +0200

    karma plugin settings migration

---
+++ application/core/Database/MigrationHelper.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace ManiaControl\Database;
+	
+use ManiaControl\Settings\SettingManager;
+use ManiaControl\ManiaControl;
+
+/**
+ * Database Migration Assistant
+ *
+ * @author steeffeen
+ * @copyright ManiaControl Copyright © 2014 ManiaControl Team
+ * @license http://www.gnu.org/licenses/ GNU General Public License, Version 3
+ */
+class MigrationHelper {
+	/*
+	 * Private Properties
+	 */
+	private $maniaControl = null;
+
+	/**
+	 * Construct Migration Helper
+	 * 
+	 * @param ManiaControl $maniaControl
+	 */
+	public function __construct(ManiaControl $maniaControl) {
+		$this->maniaControl = $maniaControl;
+	}
+
+	/**
+	 * Transfer the Settings of the given Class to a new One
+	 *
+	 * @param mixed $sourceClass
+	 * @param mixed $targetClass
+	 * @return bool
+	 */
+	public function transferSettings($sourceClass, $targetClass) {
+		$sourceClass = $this->getClass($sourceClass);
+		$targetClass = $this->getClass($targetClass);
+		
+		var_dump($sourceClass, $targetClass);
+		
+		$mysqli = $this->maniaControl->database->mysqli;
+		
+		$query = "INSERT INTO `" . SettingManager::TABLE_SETTINGS . "` (`class`, `setting`, `type`, `value`, `default`)
+				SELECT ?, `setting`, `type`, `value`, `default` FROM `" . SettingManager::TABLE_SETTINGS . "` WHERE `class` = ?;";
+		$statement = $mysqli->prepare($query);
+		if ($mysqli->error) {
+			trigger_error($mysqli->error);
+			return false;
+		}
+		$statement->bind_param('ss', $targetClass, $sourceClass);
+		if ($statement->error) {
+			trigger_error($statement->error);
+			$statement->close();
+			return false;
+		}
+		$success = $statement->execute();
+		$statement->close();
+		return $success;
+	}
+
+	/**
+	 * Get the Class of the given Object
+	 *
+	 * @param mixed $class
+	 * @return string
+	 */
+	private function getClass($class) {
+		if (is_object($class)) {
+			return get_class($class);
+		}
+		return (string) $class;
+	}
+}
