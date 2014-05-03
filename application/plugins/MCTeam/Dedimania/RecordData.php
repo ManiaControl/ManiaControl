d989efd9cde11e9bbc5ec474b220106d5f47c89b
Steffen Schröder <schteffens@gmail.com>
2014-05-04 00:36:20 +0200

    updated dedimania plugin namespace to MCTeam

---
+++ application/plugins/MCTeam/Dedimania/RecordData.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MCTeam\Dedimania;
+
+use ManiaControl\Formatter;
+
+/**
+ * ManiaControl Dedimania-Plugin Record DataStructure
+ *
+ * @author    ManiaControl Team <mail@maniacontrol.com>
+ * @copyright 2014 ManiaControl Team
+ * @license   http://www.gnu.org/licenses/ GNU General Public License, Version 3
+ */
+class RecordData {
+	/*
+	 * Public Properties
+	 */
+	public $nullRecord = false;
+	public $login = '';
+	public $nickName = '';
+	public $best = -1;
+	public $rank = -1;
+	public $maxRank = -1;
+	public $checkpoints = '';
+	public $newRecord = false;
+	public $vReplay = '';
+	public $top1GReplay = '';
+
+	/**
+	 * Construct a Record by a given Record Array
+	 *
+	 * @param array $record
+	 */
+	public function __construct($record) {
+		if (!$record) {
+			$this->nullRecord = true;
+			return;
+		}
+
+		$this->login       = $record['Login'];
+		$this->nickName    = Formatter::stripDirtyCodes($record['NickName']);
+		$this->best        = $record['Best'];
+		$this->rank        = $record['Rank'];
+		$this->maxRank     = $record['MaxRank'];
+		$this->checkpoints = $record['Checks'];
+	}
+
+	/**
+	 * Construct a new Record via its Properties
+	 *
+	 * @param string $login
+	 * @param string $nickName
+	 * @param float  $best
+	 * @param int    $checkpoints
+	 * @param bool   $newRecord
+	 */
+	public function constructNewRecord($login, $nickName, $best, $checkpoints, $newRecord = false) {
+		$this->nullRecord  = false;
+		$this->login       = $login;
+		$this->nickName    = $nickName;
+		$this->best        = $best;
+		$this->checkpoints = $checkpoints;
+		$this->newRecord   = $newRecord;
+	}
+}
