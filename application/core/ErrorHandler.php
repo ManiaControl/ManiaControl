194fd6d6414f4318c5e87cf967c0b947e3b256e4
Steffen Schröder <schteffens@gmail.com>
2014-05-06 02:49:20 +0200

    report user errors (to replace them with better logging)

--- application/core/ErrorHandler.php
+++ application/core/ErrorHandler.php
@@ -145,7 +145,7 @@ class ErrorHandler {
 		$logMessage = $message . PHP_EOL . 'File&Line: ' . $fileLine . PHP_EOL . 'Trace: ' . $traceString;
 		logMessage($logMessage);
 
-		if ($this->reportErrors && !$userError) {
+		if ($this->reportErrors) {
 			$error                    = array();
 			$error['Type']            = 'Error';
 			$error['Message']         = $message;
