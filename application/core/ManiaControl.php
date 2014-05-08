e28428f35b7390c00a5a5e5047b20ba3af0e9b4e
kremsy <lukas@esc-clan.net>
2014-05-08 21:37:40 +0200

    small vers increase

--- application/core/ManiaControl.php
+++ application/core/ManiaControl.php
@@ -44,7 +44,7 @@ class ManiaControl implements CommandListener, TimerListener {
 	/*
 	 * Constants
 	 */
-	const VERSION                     = 0.131;
+	const VERSION                     = 0.132;
 	const API_VERSION                 = '2013-04-16';
 	const MIN_DEDIVERSION             = '2014-04-02_18_00';
 	const OS_UNIX                     = 'Unix';
