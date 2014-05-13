6ac2726bb9a72bce8ca48737cbee7cb2b106c34d
Steffen Schröder <schteffens@gmail.com>
2014-05-14 00:28:07 +0200

    increased version

--- application/core/ManiaControl.php
+++ application/core/ManiaControl.php
@@ -46,7 +46,7 @@ class ManiaControl implements CommandListener, TimerListener {
 	/*
 	 * Constants
 	 */
-	const VERSION                     = '0.142';
+	const VERSION                     = '0.15';
 	const API_VERSION                 = '2013-04-16';
 	const MIN_DEDIVERSION             = '2014-04-02_18_00';
 	const OS_UNIX                     = 'Unix';
