8d2c2fb8e05ee0737918bce3df0cc2f5a11dcb07
steeffeen <mail@steeffeen.com>
2014-07-05 13:22:12 +0200

    fml update

---
+++ application/core/Libs/FML/Controls/Quads/Quad_UIConstructionBullet_Buttons.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace FML\Controls\Quads;
+
+use FML\Controls\Quad;
+
+/**
+ * Quad class for 'UIConstructionBullet_Buttons' styles
+ *
+ * @author    steeffeen <mail@steeffeen.com>
+ * @copyright FancyManiaLinks Copyright © 2014 Steffen Schröder
+ * @license   http://www.gnu.org/licenses/ GNU General Public License, Version 3
+ */
+class Quad_UIConstructionBullet_Buttons extends Quad {
+	/*
+	 * Constants
+	 */
+	const STYLE                      = 'UIConstructionBullet_Buttons';
+	const SUBSTYLE_AnimMode          = 'AnimMode';
+	const SUBSTYLE_BlocMode          = 'BlocMode';
+	const SUBSTYLE_BulletEditorMode  = 'BulletEditorMode';
+	const SUBSTYLE_Circle            = 'Circle';
+	const SUBSTYLE_Core              = 'Core';
+	const SUBSTYLE_CoreFolder        = 'CoreFolder';
+	const SUBSTYLE_DeltaMode         = 'DeltaMode';
+	const SUBSTYLE_Disc              = 'Disc';
+	const SUBSTYLE_ExplosionMode     = 'ExplosionMode';
+	const SUBSTYLE_NewBullet         = 'NewBullet';
+	const SUBSTYLE_NewRandomBullet   = 'NewRandomBullet';
+	const SUBSTYLE_Point             = 'Point';
+	const SUBSTYLE_Power             = 'Power';
+	const SUBSTYLE_PowerFolder       = 'PowerFolder';
+	const SUBSTYLE_PreviewMode       = 'PreviewMode';
+	const SUBSTYLE_RootMotionMode    = 'RootMotionMode';
+	const SUBSTYLE_SmokeDebris       = 'SmokeDebris';
+	const SUBSTYLE_SmokeDebrisFolder = 'SmokeDebrisFolder';
+	const SUBSTYLE_SoundMode         = 'SoundMode';
+	const SUBSTYLE_SphereEmpty       = 'SphereEmpty';
+	const SUBSTYLE_SphereFull        = 'SphereFull';
+	const SUBSTYLE_Trail             = 'Trail';
+	const SUBSTYLE_TrailFolder       = 'TrailFolder';
+
+	/*
+	 * Protected properties
+	 */
+	protected $style = self::STYLE;
+}
