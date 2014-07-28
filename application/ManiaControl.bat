149d8aa7f03ed17e910d793b3018372d299c84e9
Steffen Schröder <schteffens@gmail.com>
2014-07-28 14:49:43 +0200

    adjusted sample php path

--- application/ManiaControl.bat
+++ application/ManiaControl.bat
@@ -1,6 +1,6 @@
 
 REM Set the path to your php.exe here
-set phpPath="D:\xampp\php\php.exe"
+set phpPath="C:\xampp\php\php.exe"
 
 REM Start ManiaControl
 START "" /B %phpPath% -f "ManiaControl.php" 2>&1
