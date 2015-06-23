a4e8a79e1dfca251d912813de463d2560add2c40
kremsy <lukas@esc-clan.net>
2015-06-23 16:57:11 +0200

    implementation of sendChat communication

--- core/Communication/CommunicationMethods.php
+++ core/Communication/CommunicationMethods.php
@@ -12,4 +12,15 @@ namespace ManiaControl\Communication;
 interface CommunicationMethods {
 	/** Returns the last 200 lines of the chat (inclusive player logins and nicknames) */
 	const GET_SERVER_CHAT = "Chat.GetServerChat";
+
+	/** Sends a ChatMessage to the Server
+	 *  Required Params:
+	 *  - message
+	 *  Optional Params
+	 *  - prefix        (use custom prefix or false for no prefix)
+	 *  - login         (login of a receiver if the message don't get sent to all)
+	 *  - adminLevel    (minimum Admin Level if the Message should get sent to an Admin)
+	 *  - type          (type of the message (information, error, success or usage)
+	 */
+	const SEND_CHAT_MESSAGE = "Chat.SendChatMessage";
 }
