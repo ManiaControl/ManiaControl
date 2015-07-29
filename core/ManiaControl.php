6a8087cd9526df55d35b6db4d81dd4fdc4d07030
kremsy <lukas@esc-clan.net>
2015-07-29 14:35:19 +0200

    additional headers in asynchronousfilereader

--- core/ManiaControl.php
+++ core/ManiaControl.php
@@ -47,7 +47,7 @@ class ManiaControl implements CallbackListener, CommandListener, TimerListener, 
 	/*
 	 * Constants
 	 */
-	const VERSION                     = '0.161';
+	const VERSION                     = '0.162';
 	const API_VERSION                 = '2013-04-16';
 	const MIN_DEDIVERSION             = '2014-04-02_18_00';
 	const SCRIPT_TIMEOUT              = 10;
