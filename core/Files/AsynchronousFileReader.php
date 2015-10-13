ea2a417373fb7f556e12b603e08bc03e9d6105fc
kremsy <lukas@esc-clan.net>
2015-10-13 19:12:27 +0200

    file reader fix

--- core/Files/AsynchronousFileReader.php
+++ core/Files/AsynchronousFileReader.php
@@ -58,6 +58,24 @@ class AsynchronousFileReader {
 		}
 	}
 
+
+	/**
+	 * Create a new cURL Request for the given URL, DO NOT CALL MANUALLY!
+	 *
+	 * @param string $url
+	 * @return Request
+	 */
+	public function newRequest($url) {
+		$request = new Request($url);
+		$request->getOptions()->set(CURLOPT_TIMEOUT, 60)->set(CURLOPT_HEADER, false)// don't display response header
+		        ->set(CURLOPT_CRLF, true)// linux line feed
+		        ->set(CURLOPT_ENCODING, '')// accept encoding
+		        ->set(CURLOPT_USERAGENT, 'ManiaControl v' . ManiaControl::VERSION)// user-agent
+		        ->set(CURLOPT_RETURNTRANSFER, true); // return instead of output content
+		return $request;
+	}
+
+
 	/**
 	 * Load a Remote File
 	 *
