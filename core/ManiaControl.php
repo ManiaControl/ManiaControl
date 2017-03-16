f4e0579e24c6b5cc0a05cf04b38f2f517190a794
kremsy <lukas@esc-clan.net>
2017-03-16 21:45:48 +0100

    ssl update + v0.166

--- core/ManiaControl.php
+++ core/ManiaControl.php
@@ -47,11 +47,11 @@ class ManiaControl implements CallbackListener, CommandListener, TimerListener, 
 	/*
 	 * Constants
 	 */
-	const VERSION                     = '0.165';
+	const VERSION                     = '0.166';
 	const API_VERSION                 = '2013-04-16';
 	const MIN_DEDIVERSION             = '2014-04-02_18_00';
 	const SCRIPT_TIMEOUT              = 10;
-	const URL_WEBSERVICE              = 'http://ws.maniacontrol.com/';
+	const URL_WEBSERVICE              = 'https://ws.maniacontrol.com/';
 	const SETTING_PERMISSION_SHUTDOWN = 'Shutdown ManiaControl';
 	const SETTING_PERMISSION_RESTART  = 'Restart ManiaControl';
 
