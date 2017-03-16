e4f7346d4270d2d17b87ce7413e578100db9f5c4
kremsy <lukas@esc-clan.net>
2017-03-16 21:53:45 +0100

    updated icon link

--- core/Manialinks/IconManager.php
+++ core/Manialinks/IconManager.php
@@ -22,7 +22,7 @@ class IconManager implements CallbackListener {
 	/*
 	 * Constants
 	 */
-	const DEFAULT_IMG_URL = 'https://images.maniacontrol.com/icons/';
+	const DEFAULT_IMG_URL = 'https://images.maniacontrol.com/icon/';
 	const PRELOAD_MLID    = 'IconManager.Preload.MLID';
 
 	/*
