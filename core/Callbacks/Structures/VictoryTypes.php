d7b689d392c600bc870a832c3f849841f936127f
kremsy <lukas@esc-clan.net>
2017-04-07 17:37:26 +0200

    added deprecated tags

--- core/Callbacks/Structures/VictoryTypes.php
+++ core/Callbacks/Structures/VictoryTypes.php
@@ -8,6 +8,7 @@ namespace ManiaControl\Callbacks\Structures;
  * @author    ManiaControl Team <mail@maniacontrol.com>
  * @copyright 2014-2017 ManiaControl Team
  * @license   http://www.gnu.org/licenses/ GNU General Public License, Version 3
+ * @deprecated
  */
 interface VictoryTypes {
 	const TIME_LIMIT           = 1;
