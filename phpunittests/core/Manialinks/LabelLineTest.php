4ee4b20e3a76c04c948c1d6223ae54dc81493074
kremsy <lukas@esc-clan.net>
2017-04-16 15:14:48 +0200

    Added Unit Tests for the Labelline

---
+++ phpunittests/core/Manialinks/LabelLineTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lukas
+ * Date: 16. Apr. 2017
+ * Time: 14:56
+ */
+
+use ManiaControl\Manialinks\LabelLine;
+
+class LabelLineTest extends PHPUnit_Framework_TestCase {
+
+}
