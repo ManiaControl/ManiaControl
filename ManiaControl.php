9c0500f42b161dbe01292f554c4e2982aea342e9
kremsy <lukas@esc-clan.net>
2018-07-11 17:47:17 +0200

    version increase + small fix

--- ManiaControl.php
+++ ManiaControl.php
@@ -3,7 +3,7 @@
  * ManiaControl Server Controller for ManiaPlanet Server
  *
  * @author    ManiaControl Team <mail@maniacontrol.com>
- * @copyright 2014-2018 ManiaControl Team
+ * @copyright 2014-2017 ManiaControl Team
  * @license   http://www.gnu.org/licenses/ GNU General Public License, Version 3
  */
 
