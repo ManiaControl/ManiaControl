6183ae23f0a4a555880841e4109c433924e53865
Alexander Nell <axelalex2@gmx.de>
2020-02-24 17:20:01 +0100

    Update MC-Version number to 0.250

--- core/ManiaControl.php
+++ core/ManiaControl.php
@@ -53,7 +53,7 @@ class ManiaControl implements CallbackListener, CommandListener, TimerListener, 
 	/*
 	 * Constants
 	 */
-	const VERSION                     = '0.240';
+	const VERSION                     = '0.250';
 	const API_VERSION                 = '2013-04-16';
 	const MIN_DEDIVERSION             = '2017-05-03_21_00';
 	const SCRIPT_TIMEOUT              = 40;
