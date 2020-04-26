4ec8e1e1decef6990407efcf31a9c3dd6daf444a
Alexander Nell <axelalex2@gmx.de>
2020-04-26 10:09:58 +0200

    Version Bump

--- core/ManiaControl.php
+++ core/ManiaControl.php
@@ -55,7 +55,7 @@ class ManiaControl implements CallbackListener, CommandListener, TimerListener, 
 	/*
 	 * Constants
 	 */
-	const VERSION                     = '0.252';
+	const VERSION                     = '0.253';
 	const API_VERSION                 = '2013-04-16';
 	const MIN_DEDIVERSION             = '2017-05-03_21_00';
 	const SCRIPT_TIMEOUT              = 40;
